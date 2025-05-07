67fc917d6191591d03e3989a55481fbcbee551bf
Happiness549 <mhlongohappiness880@gmail.com>
2025-05-07 19:59:03 +0200

    Fix display Menu images correctly

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,29 @@
 function Contact() {
-    return <h1> Contact</h1>
+    return (
+        <div>
+    <h1> Contact</h1>
+    <div>
+        <h4>Opening Hours</h4>
+        <ul>
+            <li>Monday 08:00 - 20:00 PM</li>
+            <li>Tuesday 08:00 - 20:00 PM</li>
+            <li>Wednesday 08:00 - 20:00 PM</li>
+            <li>Thursday 08:00 - 20:00 PM</li>
+            <li>Friday 08:00 - 20:00 PM</li>
+            <li>Saturday 08:00 - 23:00 PM</li>
+            <li>Sunday 08:00 - 23:00 PM</li>
+        </ul>
+    </div>
+    <h2>Contact Us </h2>
+    <p>Have a question, feedback, or want to make a reservation? <br/> We'd love to hear from you!</p>
+    <h3>Visit Us</h3>
+    <p>123 Calorie Street, Park Town, Pt 3421</p>
+    <h3>Email us</h3>
+    <p>info@heavenlyhaven.co.za</p>
+    <h3>Call Us</h3>
+    <p>(123) 768-4579</p>
+    </div>
+    );
 
 }
 export default Contact;
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,5 +1,29 @@
 function Menu() {
-    return <h1>Menu</h1>
+    return(
+    <div>
+       <h1>Menu</h1>
+       <div className="container">
+       <div className="img">
+        <img src="/images/pizza.jpg" alt="Pizza" style={{width: '200px', height: '200px'}}/>
+        <p>R179</p>
+       </div>
+       <div className="img">
+        <img src="/images/Steak.jpg" alt="Steak" alt="Pizza" style={{width: '200px', height: '200px'}}/>
+        <p>R289</p>
+       </div>
+       <div className="img">
+        <img src="/images/Pasta.jpg" alt="Pasta" alt="Pizza" style={{width: '200px', height: '200px'}}/>
+        <p>R146</p>
+       </div>
+       <div className="img">
+        <img src="/images/Lasagne.webp" alt="Lasagne" style={{width: '200px', height: '200px'}}/>
+        <p>R230</p>
+    </div>
+       </div>
+    </div>
+    );
 }
 
 export default Menu;
+
+
